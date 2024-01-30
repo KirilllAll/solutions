180f753845e305589d0400a855f930e9e2b051e6
Kirill_Aksenov <aksenovki@gmail.com>
2024-01-30 12:22:05 +0700

    added - 349. Intersection of Two Arrays

---
+++ 349. Intersection of Two Arrays.js
@@ -0,0 +1,9 @@
+const intersection = function(nums1, nums2) {
+    const uniqArr = new Set(nums1);
+    const result = new Set();
+    for (const item of nums2){
+        uniqArr.has(item) ? result.add(item) : null;
+    }
+
+    return [...result];
+};

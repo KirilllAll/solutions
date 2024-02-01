0c18e3fc308969b9ed1883933c542d9a2bd39ae5
Ernest <ernest@MacBook-Pro.local>
2024-02-01 08:21:53 +0300

    .

--- 268. Missing Number.js
+++ 268. Missing Number.js
@@ -1,4 +1,7 @@
 // Runtime 73ms Memory 52.00 mb
+// решение за O(nlogn), можешь объяснить почему за такую сложность?
+// На самом деле, можно еще ускориться и написать решение за O(n).
+// Придумаешь такое решение?
 var missingNumber = function(nums) {
     const temp = nums.sort((a,b) => a - b);
     for(let index = 0; index <= temp.length; index++){
@@ -10,6 +13,7 @@ var missingNumber = function(nums) {
 };
 
 //263ms Memory 50.82 mb
+// решение за O(n^2). Круто, что придумал второе решение, более быстрое.
 var missingNumber2 = function(nums) {
     for (let i = 0; i <= nums.length; i++) {
       if (!nums.includes(i)) return i

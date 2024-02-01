5292596fe4e1dbc61921a45d4070b895ca1919cc
Kirill_Aksenov <aksenovki@gmail.com>
2024-02-01 22:30:53 +0700

    added solution in 268. Missing Number

--- 268. Missing Number.js
+++ 268. Missing Number.js
@@ -1,20 +1,32 @@
-// Runtime 73ms Memory 52.00 mb
-var missingNumber = function(nums) {
-    const temp = nums.sort((a,b) => a - b);
-    for(let index = 0; index <= temp.length; index++){
-        if(temp[index] !== index){
-            return index;
-        }
-    }
-
+// O(nlogn)
+var missingNumber = function (nums) {
+	const temp = nums.sort((a, b) => a - b);
+	for (let index = 0; index <= temp.length; index++) {
+		if (temp[index] !== index) {
+			return index;
+		}
+	}
 };
 
-//263ms Memory 50.82 mb
-var missingNumber2 = function(nums) {
-    for (let i = 0; i <= nums.length; i++) {
-      if (!nums.includes(i)) return i
-    }
-  };
+// O(logn)
+var missingNumber2 = function (nums) {
+	for (let i = 0; i <= nums.length; i++) {
+		if (!nums.includes(i)) return i;
+	}
+};
+
+// O(n)
+var missingNumber3 = function (nums) {
+	const hash = {};
+	for (let i = 0; i <= nums.length; i++) {
+		hash[nums[i]] = true;
+	}
+	for (let k = 0; k <= nums.length; k++) {
+		if (!hash[k]) {
+			return k;
+		}
+	}
+};
 
 // Input: nums = [3,0,1]
 // Output: 2

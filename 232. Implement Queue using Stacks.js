4160b05ecdd07f2961979837b6e8f66d5561b732
Kirill_Aksenov <aksenovki@gmail.com>
2024-02-02 12:02:25 +0700

    changed solution

--- 232. Implement Queue using Stacks.js
+++ 232. Implement Queue using Stacks.js
@@ -1,3 +1,42 @@
+// two stacks
+{
+	const MyQueue = function () {
+		this.stack1 = [];
+		this.stack2 = [];
+	};
+
+	MyQueue.prototype.push = function (x) {
+		this.stack1.push(x);
+	};
+
+	MyQueue.prototype.pop = function () {
+		if (this.isEmptyStack(this.stack2)) this.transfer();
+		return this.stack2.pop();
+	};
+
+	MyQueue.prototype.peek = function () {
+		if (this.isEmptyStack(this.stack2)) this.transfer();
+		return this.stack2[this.stack2.length - 1];
+	};
+
+	MyQueue.prototype.empty = function () {
+		return this.isEmptyStack(this.stack1) && this.isEmptyStack(this.stack2);
+	};
+
+    // utils
+    
+	MyQueue.prototype.isEmptyStack = function (stack) {
+		return !stack.length;
+	};
+
+	MyQueue.prototype.transfer = function () {
+		while (this.stack1.length > 0) {
+			this.stack2.push(this.stack1.pop());
+		}
+	};
+}
+
+// one stack 
 const MyQueue = function () {
 	this.stack = [];
 };
@@ -8,7 +47,7 @@ MyQueue.prototype.push = function (x) {
 
 MyQueue.prototype.pop = function () {
 	// что делает .shift?
-	// предполагается, что у стека есть доступ только к верхнему элементу, так что решение будет чуть сложнее. Надо использовать несколько стеков, см название задачи. 
+	// предполагается, что у стека есть доступ только к верхнему элементу, так что решение будет чуть сложнее. Надо использовать несколько стеков, см название задачи.
 	return this.stack.shift();
 };
 

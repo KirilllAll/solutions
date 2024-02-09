54a7c1b428b64c15320d42876deb5602d98a10b0
Kirill_Aksenov <aksenovki@gmail.com>
2024-02-09 17:50:26 +0700

    added flatten func

--- methods/Recursion.js
+++ methods/Recursion.js
@@ -1,5 +1,15 @@
 const fact = (num) => {
-    if(num === 0) return 1;
-    return num * fact(num - 1)
-}
-console.log(fact(5))
+	if (num === 0) return 1;
+	return num * fact(num - 1);
+};
+
+const fibonacci = (num) => {
+	if (num <= 1) {
+		return 1;
+	}
+	return fibonacci(num - 1) * fibonacci(num - 2);
+};
+
+const flatten = (nums) => {
+	return nums.reduce((acc, item) => (Array.isArray(item) ? [...acc, ...flatten(item)] : [...acc, item]), []);
+};

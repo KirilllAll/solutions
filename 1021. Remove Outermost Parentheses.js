2509cb12cb938d8fb76ea87f44ac42bf34dc0287
Kirill_Aksenov <aksenovki@gmail.com>
2024-02-17 23:59:34 +0700

    1021. Remove Outermost Parentheses added solution with stack

--- 1021. Remove Outermost Parentheses.js
+++ 1021. Remove Outermost Parentheses.js
@@ -1,17 +1,52 @@
+{
+// Solutions with depth
 const removeOuterParentheses = function (brackets) {
-    let depth = 0;
-    let resultString = '';
+	let depth = 0;
+	let resultString = '';
 
-    for (const letter of brackets){
-        if(letter === '('){
-            depth +=1;
-        }
-        if(depth > 1){
-            resultString+=letter;
-        }
-        if(letter === ')'){
-            depth -=1;
-        }
-    }
-    return resultString;
+	for (const letter of brackets) {
+		if (letter === '(') {
+			depth += 1;
+		}
+		if (depth > 1) {
+			resultString += letter;
+		}
+		if (letter === ')') {
+			depth -= 1;
+		}
+	}
+	return resultString;
 };
+}
+{
+// Solutions with stack
+const removeOuterParentheses = function (s) {
+    const stack1 = [];
+    const stack2 = [];
+    let result = '';
+    const openBracket = '(';
+    const closeBracket = ')';
+    
+    for (const letter of s) {
+        if(stack1.length === 0 && letter === openBracket){
+            stack1.push(openBracket)
+            continue
+        }
+        if(stack1.length > 0 && letter === openBracket){
+            stack2.push(openBracket)
+            result += letter;
+            continue
+        }
+        if(stack2.length > 0 && letter === closeBracket){
+            stack2.pop();
+            result += letter;
+            continue
+        } 
+        if(stack2.length === 0 && letter === closeBracket){
+            stack1.pop();
+            continue
+        } 
+    }
+    return result;
+};
+}

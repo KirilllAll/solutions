dd90eb214d9d6fa6b8674a4021f1398249118e0a
Kirill_Aksenov <aksenovki@gmail.com>
2024-02-28 17:51:06 +0700

    155. Min Stack

---
+++ leetcode/155. Min Stack.js
@@ -0,0 +1,37 @@
+var MinStack = function() {
+    this.stack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    const length = this.stack.length;
+    if(length === 0) return undefined;
+    const last = this.stack[length - 1];
+    this.stack.length = length - 1;
+    return last;
+ };
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return Math.min(...this.stack)
+};
+

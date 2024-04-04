c67e41b6a00d0baf49f49c78d4fc6727c592b916
Kirill_Aksenov <aksenovki@gmail.com>
2024-04-04 14:53:27 +1000

    144. Binary Tree Preorder Traversal

--- leetcode/144. Binary Tree Preorder Traversal.js
+++ leetcode/144. Binary Tree Preorder Traversal.js
@@ -6,15 +6,15 @@ const getRootValues = (root, data) => {
 	data.push(root.val);
 
 	if (root.left) {
-		getRoot(root.left, data);
+		getRootValues(root.left, data);
 	}
 	if (root.right) {
-		getRoot(root.right, data);
+		getRootValues(root.right, data);
 	}
 
 	return data;
 };
 
-const preorderTraversal = function (root) {
+const preOrderTraversal = function (root) {
 	return getRootValues(root, []);
 };

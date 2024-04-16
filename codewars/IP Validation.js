9c6ae76def2bee82092125476a34613e3349d9a3
Kirill_Aksenov <aksenovki@gmail.com>
2024-04-16 12:21:22 +1000

    Code Wars: IP Validation

--- codewars/IP Validation.js
+++ codewars/IP Validation.js
@@ -1,14 +1,16 @@
 function isValidIP(str) {
-    const arr = str.split(".");
-    const regex = /[!@#$%^&*.,<>/\'";:?\n' '/a-d]/;
-    if(arr.length === 4) {
+	const arr = str.split('.');
+	const regex = /[!@#$%^&*.,<>/\'";:?\n' '/aA-eE]/;
+	if (arr.length === 4) {
+		return arr.every((str) => {
+			let number = parseInt(str, 10);
 
-       return arr.every((str) => {
-        if(str.includes('e') || str.includes('E') || regex.test(str)) return false;
-        if(str.startsWith('0') && str.length > 1) return false;
-        let number = parseInt(str, 10);
-        return !isNaN(number) && number >= 0 && number <= 255;
-    })}
-   
-    return false;
-   }
+			if (regex.test(str)) return false;
+			if (str.startsWith('0') && str.length > 1) return false;
+
+			return !isNaN(number) && number >= 0 && number <= 255;
+		});
+	}
+
+	return false;
+}

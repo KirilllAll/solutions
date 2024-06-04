dce1ad04f196aee3b0e9b2a23d0bedf81fecb002
Kirill_Aksenov <aksenovki@gmail.com>
2024-06-04 14:40:45 +1000

    557.\ Reverse\ Words\ in\ a\ String\ III.js

--- leetcode/557. Reverse Words in a String III.js
+++ leetcode/557. Reverse Words in a String III.js
@@ -4,4 +4,18 @@ const reverseWords = function (s) {
 		.map((item) => item.split('').reverse().join(''))// O(s) O(item.length)
 		.join(' '); //O(s)
 };
-//[1,2,3] =>
+
+var reverseWords2 = function(s) {
+	const arr = s.split(' ');
+
+	const reverse = (arrItem) => {
+			const res = [];
+			for(let i = arrItem.length - 1; i >= 0; i -= 1){
+					res.push(arrItem[i])
+			}
+
+			return res;
+	}
+	
+ return arr.reduce((acc, item) => acc += reverse(item).join('') + ' ', '').trim('')
+};

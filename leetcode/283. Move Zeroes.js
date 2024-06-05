45a9321617c249f613bdfbbe3dafaa9fd4fbcc7f
Kirill_Aksenov <aksenovki@gmail.com>
2024-06-05 13:34:39 +1000

    283.\ Move\ Zeroes.js

--- leetcode/283. Move Zeroes.js
+++ leetcode/283. Move Zeroes.js
@@ -1,31 +1,6 @@
-// O(n^2)
-const moveZeroes = function (nums) {
-	for (let i = 0; i < nums.length; i++) {
-		if (nums[i] === 0) {
-			nums.splice(i, 1);
-			nums.push(0);
-		}
-	}
-	return nums;
-};
-
-// O(n^2)
-const moveZeroes2 = function (nums) {
-	for (let i = 0; i < nums.length; i++) {
-		let left = 0;
-		for (let i = 0; i < nums.length; i++) {
-			if (nums[i] !== 0) {
-				[nums[left], nums[i]] = [nums[i], nums[left]];
-				left++;
-			}
-		}
-	}
-	return nums;
-};
-
 
 // O(n)
-const moveZeroes3 = (nums) => {
+const moveZeroes = (nums) => {
 	let index = 0;
 	const length = nums.length;
 
@@ -44,28 +19,18 @@ const moveZeroes3 = (nums) => {
 };
 
 // O(n) one loop 
-const moveZeroes4 = function(nums) {
-    let pointer2 = 0
-    for(let pointer1 = 0; pointer1 < nums.length; pointer1++){
-        if(nums[pointer1] !== 0) {
-            [nums[pointer1], nums[pointer2]] = [nums[pointer2], nums[pointer1]];
-            pointer2 +=1
-        }
-    }
-    return nums;
-};
+function moveZeroes (nums) {
+	let pointer = 0;
 
-/**
- * 
- * 
- * 
- * Example 1:
+	const swap = (arr, first, second) => {
+		[arr[first], arr[second]] = [arr[second], arr[first]];
+	}
 
-Input: nums = [0,1,0,3,12]
-Output: [1,3,12,0,0]
-Example 2:
-
-Input: nums = [0]
-Output: [0]
-
- */
+	for(let index = 0; index < nums.length; index++){
+		if(nums[index] !== 0) {
+			swap(nums, index, pointer);
+			pointer +=1
+		}
+}
+return nums;
+}

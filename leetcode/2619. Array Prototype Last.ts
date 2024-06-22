c4c86a7242bfb0388665e7d92db80b1b9a57c9c0
Kirill_Aksenov <aksenovki@gmail.com>
2024-06-22 12:39:02 +1000

    2619. Array Prototype Last.ts

--- leetcode/2619. Array Prototype Last.ts
+++ leetcode/2619. Array Prototype Last.ts
@@ -7,3 +7,31 @@ Array.prototype.last = function () {
 
   return this[this.length - 1];
 };
+
+let a;
+let p4 = new Promise(function (resolve) {
+  console.log("TEST A1", a);
+  a = 25;
+
+  setTimeout(() => {
+    console.log("TEST A2", a);
+    resolve(a);
+  }, 100);
+});
+
+setTimeout(function timeout() {
+  a = 10;
+  console.log("TEST A3", a);
+}, 100);
+
+p4.then(function (b) {
+  console.log("TEST A4", a);
+});
+
+console.log("TEST A5", a);
+
+// a1 - undefined
+// a5 - 25
+// a2 25
+// a4 - 25
+// a3 - 10
